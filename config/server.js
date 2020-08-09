b0214dc25faa9fd61ed19480ad60c56a6cc69a9b
Marvan N <marvan.n@drivezy.com>
2020-08-09 23:11:22 +0530

    #comment saving changes

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
     admin: {
-        url: '/dashboard',
+        url: '/',
         auth: {
             secret: env('ADMIN_JWT_SECRET', '75c3a0bf67887387cab546471abcc8e7'),
         },
